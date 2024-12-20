d132342ba6bcb9fe045e32cfc31a33620d9db11e
carlosjr1227 <128106044+carlosjr1227@users.noreply.github.com>
2024-12-20 10:40:17 -0300

    Add files via upload

---
+++ area_do_trapezio.py
@@ -0,0 +1,7 @@
+#Crie um programa que calcule a area de um trapézio
+base1 = float(input("Digite o valor da base menor : \n"))
+base2 = float(input("Digite o valor da base maior : \n"))
+altura = float(input("Digite o valor da altura : \n"))
+#Calcule a area do trapézio
+area = ((base1+base2)*altura)/2
+print("A area do trapézio é : ",area)
